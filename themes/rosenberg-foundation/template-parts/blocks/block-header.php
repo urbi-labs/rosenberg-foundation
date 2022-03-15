289a321e9013a672311ec1ae940afd78f9409c29
el_gustavo <gustavowebs@gmail.com>
2022-03-14 21:32:03 -0300

    add logo and header block file

---
+++ themes/rosenberg-foundation/template-parts/blocks/block-header.php
@@ -0,0 +1,36 @@
+<div class="container header-container">
+    <a href="<?php echo get_site_url() ?>">
+        <img src="<?php echo get_template_directory_uri()  ?>/images/rosenberg-logo.svg"
+            alt="<?php echo get_bloginfo('name') ?>" class="header__logo" />
+    </a>
+    <nav class="header__navigation">
+        <ul class="header__menu">
+            <li>
+                <a href="" class="header__menu__link">
+                    Grantmaking
+                </a>
+            </li>
+            <li>
+                <a href="" class="header__menu__link">
+                    News
+                </a>
+            </li>
+            <li>
+                <a href="" class="header__menu__link">
+                    About Us
+                </a>
+
+            </li>
+            <li class="header__menu--leadingedgefund">
+                <a href="" class="header__menu__link">
+                    LEADING<span>EDGE</span>FUND
+                </a>
+            </li>
+        </ul>
+        <ul class="header__social-icons">
+            <li>
+
+            </li>
+        </ul>
+    </nav>
+</div>
