ae00f481d4f9477842550ce97e0caf6a75ff64cb
el_gustavo <gustavowebs@gmail.com>
2022-03-16 16:35:40 -0300

    pagehome padding for test

--- themes/rosenberg-foundation/front-page.php
+++ themes/rosenberg-foundation/front-page.php
@@ -1,9 +1,9 @@
 <?php
- /*
+/*
 Template Name: Home
 */
 get_header(); ?>
 <main class="page__home">
-
+    <?php get_template_part('template-parts/blocks/block', 'internal-intro-section.php'); ?>
 </main>
 <?php get_footer(); ?>
