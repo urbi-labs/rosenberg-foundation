41ba285aee397a619d9e4dc3de87f5711ef0f7e1
el_gustavo <gustavowebs@gmail.com>
2022-03-17 11:11:13 -0300

    internal intro section

--- themes/rosenberg-foundation/front-page.php
+++ themes/rosenberg-foundation/front-page.php
@@ -3,7 +3,7 @@
 Template Name: Home
 */
 get_header(); ?>
-<main class="page__home">
-    <?php get_template_part('template-parts/blocks/block', 'internal-intro-section.php'); ?>
+<main class="page__home ">
+    <?php get_template_part('template-parts/blocks/block', 'internal-intro-section'); ?>
 </main>
 <?php get_footer(); ?>
