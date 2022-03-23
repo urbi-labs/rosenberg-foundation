152d8aa649836203f288465975aa0c0d0c3b86a2
magali.escobar <rociomagaliescobar@gmail.com>
2022-03-23 01:46:32 -0300

    Create component template

---
+++ themes/rosenberg-foundation/template-parts/tpl-hero-internal.php
@@ -0,0 +1,7 @@
+<?php 
+
+?>
+
+<div class="hero-internal__container" style="background-image:url('<?php echo get_stylesheet_directory_uri() . '/images/about-us-hero-original.jpg' ?>')">
+    <img src="">       
+</div>
