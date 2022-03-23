ef97a51e74e9327ed432c983b3acf4b4a0a1ac30
magali.escobar <rociomagaliescobar@gmail.com>
2022-03-23 15:58:39 -0300

    Changes in css to look similar to mockup. Added new images and text

--- themes/rosenberg-foundation/template-parts/tpl-hero-internal.php
+++ themes/rosenberg-foundation/template-parts/tpl-hero-internal.php
@@ -2,6 +2,6 @@
 
 ?>
 
-<div class="hero-internal__container" style="background-image:url('<?php echo get_stylesheet_directory_uri() . '/images/about-us-hero-original.jpg' ?>')">
-    <img src="">       
+<div class="hero-internal__container" style="background-image:url('<?php echo get_stylesheet_directory_uri() . '/images/about-us-wb.png' ?>')">
+    <p class="hero-internal__text">Grantmaking</p>
 </div>
