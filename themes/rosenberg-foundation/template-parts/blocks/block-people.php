f547b69a5ed2f2649c0b5026dec4ad741c8c5604
el_gustavo <gustavowebs@gmail.com>
2022-03-28 17:42:44 -0300

    add field vars in block for title and button

--- themes/rosenberg-foundation/template-parts/blocks/block-people.php
+++ themes/rosenberg-foundation/template-parts/blocks/block-people.php
@@ -1,5 +1,14 @@
+<?php
+$title = get_field('title');
+$button_text = get_field('button_text');
+$button_link  = get_field('button_link');
+?>
 <div class="people-block">
-    <h2 class="h2 people-block__title">People</h2>
+    <?php
+    if ($title) { ?>
+    <h2 class="h2 people-block__title"><?php echo $title; ?></h2>
+    <?php    }
+    ?>
     <div class="people-block__list">
         <?php
         $i = 0;
@@ -15,10 +24,15 @@
         }
         ?>
     </div>
+    <?php
+    if ($button_text && $button_link) { ?>
     <div class="people-block__button__container">
-        <a href="" class="btn btn--blue btn--secondary  people-block__button">
-            Meet the people behind the foundation<img
+        <a href="<?php echo $button_link ?>" class="btn btn--blue btn--secondary  people-block__button">
+            <?php echo $button_text ?><img
                 src="<?php echo get_template_directory_uri() . "/images/icons/icon-arrow-right-small-fill-blue.svg" ?>" />
         </a>
     </div>
+    <?php    }
+    ?>
+
 </div>
