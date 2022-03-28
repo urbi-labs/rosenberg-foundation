e9c1b2afcb8791321c5e8997fd9e6de39cdc0886
Nando Delgado <nando.emmanuel@gmail.com>
2022-03-28 19:11:57 -0300

    Adjustments to overlapping cards static

--- themes/rosenberg-foundation/template-parts/blocks/block-overlapping-cards.php
+++ themes/rosenberg-foundation/template-parts/blocks/block-overlapping-cards.php
@@ -1,86 +1,59 @@
-<div class="container-cards">
-    <div class="overlapping-cards__container card-link image-right">
-        <div class="overlapping-cards__image card-link-image">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/images/michael-tubbs-article-thumbnail1.png' ?>">
-        </div>
-        <div class="overlapping-cards__text card-link-text">
-            <div class="overlapping-cards__text-lead">
-                <h3 class="overlapping-cards__text-heading">Michael Tubbs Joins Rosenberg Foundation as First Senior Fellow</h3>
-            </div>
-            <div class="overlapping-cards__read-more"><a href="#" class="overlapping-cards__read-more-link"> Read more </a></div>
-        </div>
+<div class="overlapping-cards__container card-link">
+    <div class="overlapping-cards__image card-link-image">
+        <img src="<?php echo get_stylesheet_directory_uri() . '/images/michael-tubbs-article-thumbnail1.png' ?>" class="overlapping-cards__image__image">
     </div>
-    <div class="overlapping-cards__text card-link-text-sm hidden">
+    <div class="overlapping-cards__text card-link-text">
         <div class="overlapping-cards__text-lead">
-            <p class="overlapping-cards__text-heading">Michael Tubbs Joins Rosenberg Foundation as First Senior Fellow</p>
+            <h3 class="overlapping-cards__text-heading">Michael Tubbs Joins Rosenberg Foundation as First Senior Fellow</h3>
         </div>
         <div class="overlapping-cards__read-more"><a href="#" class="overlapping-cards__read-more-link"> Read more </a></div>
     </div>
-
-    <!-- <div class="overlapping-cards__container card-expanded">
-        <div class="overlapping-cards__image card-expanded-image">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/images/leading-edge-fund-1.png' ?>">
-        </div>
-        <div class="overlapping-cards__text card-expanded-text">
-            <div class="overlapping-cards__text-lead has-read-more">
-                <p  id="overlapping-cards__text-heading" class="overlapping-cards__text-heading">Criminal and Youth Justice Reform</p>
-                <div id="overlapping-cards__text-content" class="overlapping-cards__text-content">
-                    <p>Dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. Arcu vitae elementum curabitur vitae nunc.</p>
-                    <p>Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. Imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor. Malesuada proin libero nunc consequat interdum varius sit amet mattis. Nulla pellentesque dignissim enim sit. Quisque egestas diam in arcu cursus euismod.</p>
-                </div>
-            </div>
-        </div>
+</div>
+<div class="overlapping-cards__text card-link-text-sm hidden">
+    <div class="overlapping-cards__text-lead">
+        <p class="overlapping-cards__text-heading">Michael Tubbs Joins Rosenberg Foundation as First Senior Fellow</p>
     </div>
-    <div class="overlapping-cards__text card-expanded-text-sm hidden">
+    <div class="overlapping-cards__read-more"><a href="#" class="overlapping-cards__read-more-link"> Read more </a></div>
+</div>
+
+<div class="overlapping-cards__container card-link text-left">
+    <div class="overlapping-cards__image card-link-image">
+        <img src="<?php echo get_stylesheet_directory_uri() . '/images/michael-tubbs-article-thumbnail1.png' ?>" class="overlapping-cards__image__image">
+    </div>
+    <div class="overlapping-cards__text card-link-text text-left">
+        <div class="overlapping-cards__text-lead">
+            <h3 class="overlapping-cards__text-heading">Michael Tubbs Joins Rosenberg Foundation as First Senior Fellow</h3>
+        </div>
+        <div class="overlapping-cards__read-more"><a href="#" class="overlapping-cards__read-more-link"> Read more </a></div>
+    </div>
+</div>
+<div class="overlapping-cards__text card-link-text-sm hidden">
+    <div class="overlapping-cards__text-lead">
+        <p class="overlapping-cards__text-heading">Michael Tubbs Joins Rosenberg Foundation as First Senior Fellow</p>
+    </div>
+    <div class="overlapping-cards__read-more"><a href="#" class="overlapping-cards__read-more-link"> Read more </a></div>
+</div>
+
+<!-- <div class="overlapping-cards__container card-expanded">
+    <div class="overlapping-cards__image card-expanded-image">
+        <img src="<?php echo get_stylesheet_directory_uri() . '/images/leading-edge-fund-1.png' ?>">
+    </div>
+    <div class="overlapping-cards__text card-expanded-text">
         <div class="overlapping-cards__text-lead has-read-more">
-            <p class="overlapping-cards__text-heading">Criminal and Youth Justice Reform</p>
-            <div class="overlapping-cards__text-content">
+            <p  id="overlapping-cards__text-heading" class="overlapping-cards__text-heading">Criminal and Youth Justice Reform</p>
+            <div id="overlapping-cards__text-content" class="overlapping-cards__text-content">
                 <p>Dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. Arcu vitae elementum curabitur vitae nunc.</p>
                 <p>Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. Imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor. Malesuada proin libero nunc consequat interdum varius sit amet mattis. Nulla pellentesque dignissim enim sit. Quisque egestas diam in arcu cursus euismod.</p>
             </div>
         </div>
-    </div> -->
+    </div>
 </div>
-<div class="container-cards">
-    <div class="overlapping-cards__container card-link text-left">
-        <div class="overlapping-cards__image card-link-image">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/images/michael-tubbs-article-thumbnail1.png' ?>">
-        </div>
-        <div class="overlapping-cards__text card-link-text text-left">
-            <div class="overlapping-cards__text-lead">
-                <h3 class="overlapping-cards__text-heading">Michael Tubbs Joins Rosenberg Foundation as First Senior Fellow</h3>
-            </div>
-            <div class="overlapping-cards__read-more"><a href="#" class="overlapping-cards__read-more-link"> Read more </a></div>
+<div class="overlapping-cards__text card-expanded-text-sm hidden">
+    <div class="overlapping-cards__text-lead has-read-more">
+        <p class="overlapping-cards__text-heading">Criminal and Youth Justice Reform</p>
+        <div class="overlapping-cards__text-content">
+            <p>Dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. Arcu vitae elementum curabitur vitae nunc.</p>
+            <p>Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. Imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor. Malesuada proin libero nunc consequat interdum varius sit amet mattis. Nulla pellentesque dignissim enim sit. Quisque egestas diam in arcu cursus euismod.</p>
         </div>
     </div>
-    <div class="overlapping-cards__text card-link-text-sm hidden">
-        <div class="overlapping-cards__text-lead">
-            <p class="overlapping-cards__text-heading">Michael Tubbs Joins Rosenberg Foundation as First Senior Fellow</p>
-        </div>
-        <div class="overlapping-cards__read-more"><a href="#" class="overlapping-cards__read-more-link"> Read more </a></div>
-    </div>
-
-    <!-- <div class="overlapping-cards__container card-expanded">
-        <div class="overlapping-cards__image card-expanded-image">
-            <img src="<?php echo get_stylesheet_directory_uri() . '/images/leading-edge-fund-1.png' ?>">
-        </div>
-        <div class="overlapping-cards__text card-expanded-text">
-            <div class="overlapping-cards__text-lead has-read-more">
-                <p  id="overlapping-cards__text-heading" class="overlapping-cards__text-heading">Criminal and Youth Justice Reform</p>
-                <div id="overlapping-cards__text-content" class="overlapping-cards__text-content">
-                    <p>Dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. Arcu vitae elementum curabitur vitae nunc.</p>
-                    <p>Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. Imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor. Malesuada proin libero nunc consequat interdum varius sit amet mattis. Nulla pellentesque dignissim enim sit. Quisque egestas diam in arcu cursus euismod.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="overlapping-cards__text card-expanded-text-sm hidden">
-        <div class="overlapping-cards__text-lead has-read-more">
-            <p class="overlapping-cards__text-heading">Criminal and Youth Justice Reform</p>
-            <div class="overlapping-cards__text-content">
-                <p>Dolor sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. Arcu vitae elementum curabitur vitae nunc.</p>
-                <p>Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget. Imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor. Malesuada proin libero nunc consequat interdum varius sit amet mattis. Nulla pellentesque dignissim enim sit. Quisque egestas diam in arcu cursus euismod.</p>
-            </div>
-        </div>
-    </div> -->
-</div>
+</div> -->
