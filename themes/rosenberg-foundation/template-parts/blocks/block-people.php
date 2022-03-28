1132b3e67742ead490139fedb85d0829dcb03d38
el_gustavo <gustavowebs@gmail.com>
2022-03-28 16:24:04 -0300

    init block html for people, add title h2 and margin bottom

---
+++ themes/rosenberg-foundation/template-parts/blocks/block-people.php
@@ -0,0 +1,5 @@
+<div class="people-block">
+    <h2 class="h2 people-block__title">People</h2>
+    <div class="people-block-list"></div>
+    <a href="" class="btn btn-secondary btn--blue"></a>
+</div>
