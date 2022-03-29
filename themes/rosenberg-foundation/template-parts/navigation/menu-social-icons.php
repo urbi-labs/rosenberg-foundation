5e1040113876735ae707b973020b88aaf7a49180
Nando Delgado <nando.emmanuel@gmail.com>
2022-03-29 18:32:01 -0300

    Adjustments to header and footer social icons

--- themes/rosenberg-foundation/template-parts/navigation/menu-social-icons.php
+++ themes/rosenberg-foundation/template-parts/navigation/menu-social-icons.php
@@ -1,18 +1,22 @@
 <?php
 
+$social_profiles = $args['social_profiles'];
 $twitter_url = get_field('twitter', 'option');
 $facebook_url = get_field('facebook', 'option');
 $youtube_url = get_field('youtube', 'option');
 $color = isset($args['color']) ? "white" : "black";
 ?>
-<ul class="menu-social-icons">
 
+<?php if(array_filter($social_profiles)) : ?>
+<ul class="menu-social-icons">
     <?php
     if ($facebook_url) { ?>
     <li>
-        <a href="<?php echo esc_url($facebook_url); ?>">
+        <a href="<?php echo esc_url($facebook_url); ?>" target="_blank">
             <img
-                src="<?php echo get_template_directory_uri()  ?>/images/social-icons/facebook-container<?php echo $color == "white" ? "-white" : "" ?>.svg" />
+            class="menu-social-icons__icon"
+            src="<?php echo get_template_directory_uri()  ?>/images/social-icons/facebook-container<?php echo $color == "white" ? "-white" : "" ?>.svg"
+            />
         </a>
     </li>
 
@@ -22,9 +26,11 @@ $color = isset($args['color']) ? "white" : "black";
     if ($twitter_url) { ?>
 
     <li>
-        <a href="<?php echo esc_url($twitter_url); ?>">
+        <a href="<?php echo esc_url($twitter_url); ?>" target="_blank">
             <img
-                src="<?php echo get_template_directory_uri()  ?>/images/social-icons/twitter-container<?php echo  $color == "white" ? "-white" : "" ?>.svg" />
+            class="menu-social-icons__icon"
+            src="<?php echo get_template_directory_uri()  ?>/images/social-icons/twitter-container<?php echo  $color == "white" ? "-white" : "" ?>.svg"
+            />
         </a>
     </li>
     <?php    }
@@ -33,12 +39,15 @@ $color = isset($args['color']) ? "white" : "black";
     if ($youtube_url) { ?>
 
     <li>
-        <a href="<?php echo esc_url($youtube_url) ?>">
+        <a href="<?php echo esc_url($youtube_url) ?>" target="_blank">
             <img
-                src="<?php echo get_template_directory_uri()  ?>/images/social-icons/youtube-container<?php echo $color == "white" ? "-white" : "" ?>.svg" />
+            class="menu-social-icons__icon"
+            src="<?php echo get_template_directory_uri()  ?>/images/social-icons/youtube-container<?php echo $color == "white" ? "-white" : "" ?>.svg"
+            />
         </a>
     </li>
     <?php    }
     ?>
 
 </ul>
+<?php endif; ?>
