62dd3c0f38e77f7e2907a5fea7cb16d6da6dadd8
el_gustavo <gustavowebs@gmail.com>
2022-03-29 15:37:25 -0300

    block people

--- themes/rosenberg-foundation/template-parts/blocks/block-people.php
+++ themes/rosenberg-foundation/template-parts/blocks/block-people.php
@@ -36,19 +36,7 @@ if ($people) { ?>
         </div>
         <?php    }
             ?>
-        <?php
-            $i = 0;
-            while ($i < 4) { ?>
-        <div class="people-block__item">
-            <img src="<?php echo get_template_directory_uri() . "/images/timothy1.jpg" ?>" alt=""
-                class="people-block__item__image" />
-            <h5 class="h5 people-block__item__name">
-                Timothy P. Silard</h5>
-            <p class="people-block__item__position">President</p>
-        </div>
-        <?php $i++;
-            }
-            ?>
+
     </div>
     <?php
         if ($button_text && $button_link) { ?>
