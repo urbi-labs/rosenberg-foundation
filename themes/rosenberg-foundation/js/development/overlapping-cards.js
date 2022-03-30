b782ee1121a636a386b7cc7aad994602bbd62221
Nando Delgado <nando.emmanuel@gmail.com>
2022-03-30 17:49:22 -0300

    Correcetd expandable card animation

--- themes/rosenberg-foundation/js/development/overlapping-cards.js
+++ themes/rosenberg-foundation/js/development/overlapping-cards.js
@@ -2,7 +2,7 @@ $(document).ready( function() {
 
     $('.read-more__expand').click( e => {
         e.preventDefault()
-        $(e.target).siblings('.overlapping-cards__text-excerpt').toggleClass('expanded')
+        $(e.target).closest('.overlapping-cards__text').find('.overlapping-cards__text-excerpt').toggleClass('expanded')
         $(e.target).closest('.overlapping-cards__text').toggleClass('expanded')
     })
 
