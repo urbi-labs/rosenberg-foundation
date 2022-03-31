9c54aa6d2f7382eb0ed44af460aa4d8185fc6673
el_gustavo <gustavowebs@gmail.com>
2022-03-31 17:10:43 -0300

    add acf support in block

--- themes/rosenberg-foundation/template-parts/blocks/block-feature-slider.php
+++ themes/rosenberg-foundation/template-parts/blocks/block-feature-slider.php
@@ -3,50 +3,38 @@
 /** feature slider block has javascript file
  * 
  */
-
+$slider = get_field('slider');
+//var_dump($slider);
 
 ?>
+<?php if ($slider && !empty($slider)) : ?>
 <div class="feature-slider__container">
     <div class="feature-slider">
 
         <?php
-        $i = 0;
-        while ($i < 5) :
-        ?>
+            foreach ($slider as $slide) :
+                $image = $slide['image'];
+
+            ?>
         <div class="feature-slider__slide">
             <div class="features-slider__slide__image">
-                <img src="<?php echo get_template_directory_uri() ?>/images/community-meeting-space-1.jpg">
+                <img src="<?php echo esc_url($image['url']) ?>" alt="<?php esc_html($slide['title']) ?>" />
             </div>
             <div class="feature-slider__slide__content">
                 <div class="feature-slider__slide__title">
                     <div class="title__orange-bg__container">
-                        <span class="h2 title__orange-bg"> Community Meeting
-                            Space</span>
+                        <span class="h2 title__orange-bg"> <?php echo esc_html($slide['title']); ?></span>
                     </div>
 
                 </div>
                 <div class="feature-slider__slide__text">
-                    Donec massa sapien faucibus et molestie ac feugiat sed lectus. Egestas tellus rutrum tellus
-                    pellentesque
-                    eu tincidunt tortor. Tempor orci dapibus ultrices in iaculis nunc sed.
-                    Vitae nunc sed velit dignissim sodales ut eu sem. Augue lacus viverra vitae congue eu consequat
-                    Jonathan@Rosenfound.org tellus mauris a diam maecenas sed enim ut:
-                    Venenatis tellus in metus
-                    Egestas tellus rutrum tellus pellentesque
-                    Tempor orci dapibus
-                    Donec massa sapien faucibus et molestie ac feugiat sed lectus. Egestas tellus rutrum tellus
-                    pellentesque
-                    eu tincidunt tortor. Tempor orci dapibus ultrices in iaculis nunc sed.
-                    Vitae nunc sed velit dignissim sodales ut eu sem. Augue lacus viverra vitae congue eu consequat
-                    Jonathan@Rosenfound.org tellus mauris a diam maecenas sed enim ut:
-                    Venenatis tellus in metus
-                    Egestas tellus rutrum tellus pellentesque
-                    Tempor orci dapibus
+                    <?php
+                            echo $slide['text'];
+                            ?>
                 </div>
             </div>
         </div>
-        <?php $i++;
-        endwhile; ?>
+        <?php endforeach; ?>
 
     </div>
     <button class="feature-slider__prev-arrow slick-prev" type="button">
@@ -56,3 +44,4 @@
         <img src="<?php echo get_template_directory_uri() ?>/images/icon-next-fill.png">
     </button>
 </div>
+<?php endif; ?>
