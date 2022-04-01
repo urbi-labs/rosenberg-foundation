0fb7a4d4844e71f3e9cd480f6b5fc777ed5a0e06
el_gustavo <gustavowebs@gmail.com>
2022-04-01 18:33:14 -0300

    add setting for breakpoint phone widt, disable autoplay

--- themes/rosenberg-foundation/js/scripts.js
+++ themes/rosenberg-foundation/js/scripts.js
@@ -15,7 +15,15 @@ $(document).ready(function () {
         asNavFor: '.feature-slider__content__container',
         prevArrow: $('.feature-slider__prev-arrow.slick-prev'),
         nextArrow: $('.feature-slider__next-arrow.slick-next'),
-        autoplay: true
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    autoplay: false
+                }
+            },
+        ]
     });
 
     $('.feature-slider__content__container').slick({
