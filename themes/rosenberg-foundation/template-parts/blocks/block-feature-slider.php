ab2267664b50a0726ed9a4e6acfeface343ded18
el_gustavo <gustavowebs@gmail.com>
2022-04-01 18:25:42 -0300

    add orange bg title element

--- themes/rosenberg-foundation/template-parts/blocks/block-feature-slider.php
+++ themes/rosenberg-foundation/template-parts/blocks/block-feature-slider.php
@@ -9,39 +9,39 @@ $slider = get_field('slider');
 ?>
 <?php if ($slider && !empty($slider)) : ?>
 <div class="feature-slider__container">
-    <div class="feature-slider">
-
-        <?php
-            foreach ($slider as $slide) :
-                $image = $slide['image'];
-
-            ?>
-        <div class="feature-slider__slide">
-            <div class="features-slider__slide__image">
-                <img src="<?php echo esc_url($image['url']) ?>" alt="<?php esc_html($slide['title']) ?>" />
+    <div class="feature-slider__image__container">
+        <div class="feature-slider__image">
+            <?php
+                foreach ($slider as $slide) : ?>
+            <div class="feature-slider__image__slide">
+                <img src="<?php echo $slide['image']['url'] ?>" />
             </div>
-            <div class="feature-slider__slide__content">
-                <div class="feature-slider__slide__title">
-                    <div class="title__orange-bg__container">
-                        <span class="h2 title__orange-bg"> <?php echo esc_html($slide['title']); ?></span>
-                    </div>
+            <?php endforeach;
+                ?>
 
-                </div>
-                <div class="feature-slider__slide__text">
-                    <?php
-                            echo $slide['text'];
-                            ?>
-                </div>
+        </div>
+        <button class="feature-slider__prev-arrow slick-prev" type="button">
+            <img src="<?php echo get_template_directory_uri() ?>/images/icon-prev-fill.png" />
+        </button>
+        <button class="feature-slider__next-arrow slick-next" type="button">
+            <img src="<?php echo get_template_directory_uri() ?>/images/icon-next-fill.png" />
+        </button>
+    </div>
+    <div class="feature-slider__content__container">
+        <?php
+            foreach ($slider as $slide) : ?>
+        <div class="feature-slider__content">
+            <div class="title__orange-bg__container">
+                <span class="h2 title__orange-bg "><?php echo $slide['title'] ?></span>
+            </div>
+            <div class="feature-slider_content__text">
+                <?php echo $slide['text']; ?>
             </div>
         </div>
-        <?php endforeach; ?>
-
+        <?php endforeach;
+            ?>
     </div>
-    <button class="feature-slider__prev-arrow slick-prev" type="button">
-        <img src="<?php echo get_template_directory_uri() ?>/images/icon-prev-fill.png">
-    </button>
-    <button class="feature-slider__next-arrow slick-next" type="button">
-        <img src="<?php echo get_template_directory_uri() ?>/images/icon-next-fill.png">
-    </button>
+
+
 </div>
 <?php endif; ?>
