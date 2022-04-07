1ae7878a1858868213f21657c2bf31c30847a8c6
el_gustavo <gustavowebs@gmail.com>
2022-04-07 17:19:49 -0300

    init news page

---
+++ themes/rosenberg-foundation/news.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: News
+*/
+get_header();
+?>
+<main class="page__news">
+    <h1 class="accessible-text"><?php the_title(); ?></h1>
+    <div class="container">
+        <div class="entry-content">
+
+        </div>
+    </div>
+</main>
+<?php get_footer(); ?>
