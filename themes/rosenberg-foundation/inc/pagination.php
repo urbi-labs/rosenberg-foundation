300dacfb20ba9e7e66c2e0111f816c6eab7fb8e8
el_gustavo <gustavowebs@gmail.com>
2022-04-08 16:29:33 -0300

    pagination news

--- themes/rosenberg-foundation/inc/pagination.php
+++ themes/rosenberg-foundation/inc/pagination.php
@@ -1,34 +1,73 @@
 <?php
-function user_pagination($max_num_pages, $found_posts, $paged){
+function user_pagination($max_num_pages, $found_posts, $paged)
+{
 
-    if( $found_posts > 1 ):
-        ?>
-        <nav class="pagination" role="navigation">
-            <?php
+    if ($found_posts > 1) :
+?>
+<nav class="pagination" role="navigation">
+    <?php
             $big = 999999999; // need an unlikely integer
-            echo paginate_links( array(
-                'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+            echo paginate_links(array(
+                'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
                 'format' => '?paged=%#%',
-                'current' => max( 1, $paged ),
+                'current' => max(1, $paged),
                 'total' => $max_num_pages,
                 'end_size' => 0,
-                'start_size' => 1,
+                'mid_size' => 0,
                 'prev_text' => 'PREVIOUS',
                 'next_text' => 'NEXT',
                 'add_args' => false,
                 'type' => 'list'
-            ) );
+            ));
             ?>
-        </nav>
-        <?php
+</nav>
+<?php
     endif;
-
 }
 
-add_action( 'pre_get_posts' ,'wpse222471_query_post_type_portofolio', 1, 1 );
-function wpse222471_query_post_type_portofolio( $query ) {
-    if ( ! is_admin() && $query->is_main_query() ) {
-        $query->set( 'posts_per_page', 9 ); //set query arg ( key, value )
+
+function news_pagination($max_num_pages,  $found_posts, $paged)
+{ ?>
+<?php if ($max_num_pages) :
+
+    ?>
+<nav class="pagination" role="pagination">
+    <ul>
+        <?php
+                //current page isn't the first page
+                if ($paged > 1) : ?>
+        <li>
+            <a href="<?php echo get_pagenum_link(1);  ?>">First</a>
+        </li>
+        <li>
+            <a href="<?php echo get_pagenum_link($paged - 1);  ?>">Previous</a>
+        </li>
+
+        <?php endif; ?>
+
+        <?php
+                if ($paged < $max_num_pages) : ?>
+
+        <li>
+            <a href="<?php echo get_pagenum_link($paged + 1);  ?>">Next</a>
+        </li>
+        <li>
+            <a href="<?php echo get_pagenum_link($max_num_pages);  ?>">Last</a>
+        </li>
+
+        <?php endif;
+                ?>
+    </ul>
+</nav>
+<?php endif; ?>
+<?php }
+
+
+add_action('pre_get_posts', 'wpse222471_query_post_type_portofolio', 1, 1);
+function wpse222471_query_post_type_portofolio($query)
+{
+    if (!is_admin() && $query->is_main_query()) {
+        $query->set('posts_per_page', 9); //set query arg ( key, value )
 
         return;
     }
--- themes/rosenberg-foundation/tpl-news.php
+++ themes/rosenberg-foundation/tpl-news.php
@@ -15,138 +15,168 @@ $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 $args_recenpost = array(
     'orderby' => 'date',
     'order' => 'desc',
-    'posts_per_page' => 3,
+    'posts_per_page' => 1,
     'paged' => $paged
 
 );
-
-$recent_posts = new WP_Query($args_recenpost);
-
 ?>
 <main class="page__news">
 
-    <div class="container">
-        <div class="page__news__header">
-            <h1><?php the_title(); ?></h1>
-            <?php get_search_form(array(
-                "echo" => true,
-                "aria_label" => 'Search news'
-            )); ?>
+    <div class="page__news__header">
+        <h1><?php the_title(); ?></h1>
+        <?php get_search_form(array(
+            "echo" => true,
+            "aria_label" => 'Search news'
+        )); ?>
+    </div>
+    <?php
+    //if there isn't post show 
+    if (!$recent_posts->have_posts()) : ?>
+    <div class="news__nopost">
+        <h3>No results were found.</h3>
+    </div>
+    <?php else : /* show last 12 post and last post like internal intro section */
+        $image = wp_get_attachment_image_srcset(get_post_thumbnail_id($last_post[0]["ID"]));
+
+    ?>
+    <div class="internal-intro-section news__last-post ">
+        <div class="internal-intro-section__img news__last-post__img">
+            <img <?php echo $image ? "srcset='" . $image . "'" : "src='" . $placeholder . "'"   ?>
+                alt="<?php echo esc_html($last_post[0]["post_title"]) ?>" />
         </div>
-        <?php
-        //if there isn't post show 
-        if (!$recent_posts->have_posts()) : ?>
-        <div class="news__nopost">
-            <h3>No results were found.</h3>
-        </div>
-        <?php else : /* show last 12 post and last post like internal intro section */
-            $image = wp_get_attachment_image_srcset(get_post_thumbnail_id($last_post[0]["ID"]));
-        ?>
-        <div class="internal-intro-section news__last-post ">
-            <div class="internal-intro-section__img news__last-post__img">
-                <img srcset="<?php echo $image  ?>" alt="<?php echo esc_html($last_post[0]["post_title"]) ?>" />
+        <div class="internal-intro-section__content news__last-post__content">
+
+            <div class="news__last-post__container-text">
+                <h3 class="h3"><?php echo esc_html($last_post[0]["post_title"]); ?></h3>
+                <p>
+                    <a href="<?php echo get_permalink($last_post[0]['ID']); ?>">Read more <img
+                            src="<?php echo get_template_directory_uri() ?>/images/icons/icon-arrow-right-small-fill.svg"></a>
+                </p>
             </div>
-            <div class="internal-intro-section__content news__last-post__content">
-
-                <div class="news__last-post__container-text">
-                    <h3 class="h3"><?php echo esc_html($last_post[0]["post_title"]); ?></h3>
-                    <p>
-                        <a href="<?php echo get_permalink($last_post[0]['ID']); ?>">Read more <img
-                                src="<?php echo get_template_directory_uri() ?>/images/icons/icon-arrow-right-small-fill.svg"></a>
-                    </p>
-                </div>
 
 
-            </div>
         </div>
-        <?php
-            if ($categories) : ?>
-        <div class="news__list-categories">
-            <ul class="news__list-categories__list">
-                <li class="news__list-categories__list__item news__list-categories__list__item--active">
-                    <a href="<?php get_the_permalink($post->ID); ?>">All</a>
-                </li>
-                <?php
-                        foreach ($categories as $category) :
-
-                        ?>
-                <li class="news__list-categories__list__item ">
-                    <a href="<?php echo get_category_link($category) ?>">
-                        <?php echo $category->name; ?>
-                    </a>
-                </li>
-                <?php endforeach ?>
-            </ul>
-        </div>
-        <?php
-                ?>
-        <?php endif
-            ?>
-
-        <div class="news__list">
+    </div>
+    <?php
+        if ($categories) : ?>
+    <div class="news__list-categories">
+        <ul class="news__list-categories__list">
+            <li class="news__list-categories__list__item news__list-categories__list__item--active">
+                <a href="<?php get_the_permalink($post->ID); ?>">All</a>
+            </li>
             <?php
-                while ($recent_posts->have_posts()) :
-                    $recent_posts->the_post();
-                    $image = get_the_post_thumbnail_url($recent_posts->post->ID, 'medium');
+                    foreach ($categories as $category) :
 
-                    $excerpt =  get_the_excerpt($recent_posts->post->ID) ? get_the_excerpt($recent_posts->post->ID) : substr(get_the_content(null, null, $recent_posts->post->ID), 0, 200);
-                    $excerpt  = wp_strip_all_tags($excerpt);
-                    $new_categories = wp_get_post_categories($recent_posts->post->ID);
-                    $author = get_the_author_meta("first_name") . " " .  get_the_author_meta("last_name");
-                    if ($author) {
-                        $author =  get_the_author_meta("display_name");
-                    }
+                    ?>
+            <li class="news__list-categories__list__item ">
+                <a href="<?php echo get_category_link($category) ?>">
+                    <?php echo $category->name; ?>
+                </a>
+            </li>
+            <?php endforeach ?>
+        </ul>
+    </div>
+    <?php
+            ?>
+    <?php endif
+        ?>
 
-                ?>
-            <div class="card new__list__item">
-                <div class="card__post-featured-image" style="background-image:url('<?php echo $image; ?>')">
+    <div class="news__list">
+        <?php
+            while ($recent_posts->have_posts()) :
+                $recent_posts->the_post();
+                $image =  wp_get_attachment_image_srcset(get_post_thumbnail_id($recent_posts->post->ID));
 
-                </div>
-                <div class="news__item__content">
-                    <div class="card__post-categories new__item__categories">
+                $excerpt =  get_the_excerpt($recent_posts->post->ID) ? get_the_excerpt($recent_posts->post->ID) : substr(get_the_content(null, null, $recent_posts->post->ID), 0, 200);
+                $excerpt  = wp_strip_all_tags($excerpt);
+                $new_categories = wp_get_post_categories($recent_posts->post->ID);
+                $author = get_the_author_meta("first_name") . " " .  get_the_author_meta("last_name");
+                if ($author) {
+                    $author =  get_the_author_meta("display_name");
+                }
+                $image = false;
+
+            ?>
+        <div class="card new__list__item">
+            <div class="card__post-featured-image news__featured-image">
+                <a href="<?php the_permalink(); ?>">
+                    <img <?php echo $image ? "srcset='" . $image . "'" : "src='" . $placeholder . "'"   ?>
+                        alt="<?php echo get_the_title($recent_posts->post->ID); ?>"
+                        class="<?php echo !$image ? "news__placeholder" : "" ?>">
+                </a>
+            </div>
+            <div class="news__item__content">
+                <div class="card__post-categories new__item__categories">
+                    <?php
+                            if ($new_categories) : ?>
+                    <ul>
                         <?php
-                                if ($new_categories) : ?>
-                        <ul>
-                            <?php
-                                        foreach ($new_categories as $category_id) :
-                                            $category = get_category($category_id);
-                                        ?>
+                                    foreach ($new_categories as $category_id) :
+                                        $category = get_category($category_id);
+                                    ?>
 
-                            <li class=" ">
-                                <a href="<?php echo get_category_link($category) ?>">
-                                    <?php echo $category->name; ?>
-                                </a>
-                            </li>
-                            <?php endforeach; ?>
-                        </ul>
-                        <?php endif;
+                        <li class=" ">
+                            <a href="<?php echo get_category_link($category) ?>">
+                                <?php echo $category->name; ?>
+                            </a>
+                        </li>
+                        <?php endforeach; ?>
+                    </ul>
+                    <?php endif;
 
-                                ?>
-                    </div>
-                    <div class="card__post-title">
-                        <a class="h4 new__title"
-                            href="<?php echo get_the_permalink() ?>"><?php echo get_the_title() ?></a>
-                    </div>
-                    <div class="card__post-data">
-                        <?php echo $author ?>
-                        &#183;
-                        <?php echo get_the_date("F d, Y") ?>
-                    </div>
-                    <div class="card__post-excerpt">
-                        <?php echo $excerpt; ?>
-                    </div>
+                            ?>
+                </div>
+                <div class="card__post-data">
+                    <?php echo $author ?>
+                    &#183;
+                    <?php echo get_the_date("F d, Y") ?>
+                </div>
+                <div class="card__post-title">
+                    <a class="h4 new__title" href="<?php echo get_the_permalink() ?>"><?php echo get_the_title() ?></a>
+                </div>
+
+                <div class="card__post-excerpt">
+                    <?php echo $excerpt; ?>
                 </div>
             </div>
-            <?php endwhile; ?>
         </div>
-        <?php $max_num_pages = $recent_posts->max_num_pages;
-            if ($max_num_pages > 1) : ?>
+        <?php endwhile; ?>
+    </div>
+    <?php $max_num_pages = $recent_posts->max_num_pages;
+
+        echo $max_num_pages . "<br/>";
+        echo $paged;
+
+        if ($max_num_pages > 1) :
+
+            $big = 999999999; // need an unlikely integer
+            $pages =  paginate_links(array(
+                'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+                'format' => '?paged=%#%',
+                'current' => max(1, $paged),
+                'total' => $max_num_pages,
+                'end_size' => 2,
+                'start_size' => 1,
+                'prev_text' => '',
+                'next_text' => '',
+                'add_args' => false,
+                'type' => 'array'
+            ));
+            print_r($pages);
+        ?>
+    <div class="news_pagination">
+        <?php news_pagination($max_num_pages, $recent_posts->found_posts, $paged == 0 ? 1 : $paged);; ?>
+    </div>
+    <div class="news__pagination">
+
         <?php user_pagination($max_num_pages, $recent_posts->found_posts, $paged == 0 ? 1 : $paged); ?>
+    </div>
 
-        <?php endif; ?>
 
-        <?php wp_reset_postdata();  ?>
-        <?php endif; ?>
+    <?php endif; ?>
+
+    <?php wp_reset_postdata();  ?>
+    <?php endif; ?>
     </div>
 </main>
 <?php get_footer(); ?>
