00e5bc3f794a448a9cbffdeab9a653aaeb95879c
Nando Delgado <nando.emmanuel@gmail.com>
2022-04-09 20:38:05 -0300

    Fixed mereg conflict

--- themes/rosenberg-foundation/tpl-news.php
+++ themes/rosenberg-foundation/tpl-news.php
@@ -83,68 +83,77 @@ $placeholder = get_template_directory_uri() . '/images/rosenberg-logo.svg';
     <?php endif
         ?>
 
-    <div class="news__list">
+    <<<<<<< HEAD <div class="news__list">
         <?php
             while ($recent_posts->have_posts()) :
                 $recent_posts->the_post();
                 $image =  wp_get_attachment_image_srcset(get_post_thumbnail_id($recent_posts->post->ID));
+?>
+        <div class="news__list">
+            <?php
+                while ($recent_posts->have_posts()) :
+                    $recent_posts->the_post();
+                    $image = get_the_post_thumbnail_url($recent_posts->post->ID, 'medium');
 
-                $excerpt =  get_the_excerpt($recent_posts->post->ID) ? get_the_excerpt($recent_posts->post->ID) : substr(get_the_content(null, null, $recent_posts->post->ID), 0, 200);
-                $excerpt  = wp_strip_all_tags($excerpt);
-                $new_categories = wp_get_post_categories($recent_posts->post->ID);
-                $author = get_the_author_meta("first_name") . " " .  get_the_author_meta("last_name");
-                if ($author) {
-                    $author =  get_the_author_meta("display_name");
-                }
+                    $excerpt =  get_the_excerpt($recent_posts->post->ID) ? get_the_excerpt($recent_posts->post->ID) : substr(get_the_content(null, null, $recent_posts->post->ID), 0, 200);
+                    $excerpt  = wp_strip_all_tags($excerpt);
+                    $new_categories = wp_get_post_categories($recent_posts->post->ID);
+                    $author = get_the_author_meta("first_name") . " " .  get_the_author_meta("last_name");
+                    if ($author) {
+                        $author =  get_the_author_meta("display_name");
+                    }
+
+ 
 
 
             ?>
-        <div class="card new__list__item">
-            <div class="card__post-featured-image news__featured-image">
-                <a href="<?php the_permalink(); ?>">
-                    <img <?php echo $image ? "srcset='" . $image . "'" : "src='" . $placeholder . "'"   ?>
-                        alt="<?php echo get_the_title($recent_posts->post->ID); ?>"
-                        class="<?php echo !$image ? "news__placeholder" : "" ?>">
-                </a>
-            </div>
-            <div class="news__item__content">
-                <div class="card__post-categories new__item__categories">
-                    <?php
-                            if ($new_categories) : ?>
-                    <ul>
+            <div class="card new__list__item">
+                <div class="card__post-featured-image news__featured-image">
+                    <a href="<?php the_permalink(); ?>">
+                        <img <?php echo $image ? "srcset='" . $image . "'" : "src='" . $placeholder . "'"   ?>
+                            alt="<?php echo get_the_title($recent_posts->post->ID); ?>"
+                            class="<?php echo !$image ? "news__placeholder" : "" ?>">
+                    </a>
+                </div>
+                <div class="news__item__content">
+                    <div class="card__post-categories new__item__categories">
                         <?php
+                            if ($new_categories) : ?>
+                        <ul>
+                            <?php
                                     foreach ($new_categories as $category_id) :
                                         $category = get_category($category_id);
                                     ?>
 
-                        <li class=" ">
-                            <a href="<?php echo get_category_link($category) ?>">
-                                <?php echo $category->name; ?>
-                            </a>
-                        </li>
-                        <?php endforeach; ?>
-                    </ul>
-                    <?php endif;
+                            <li class=" ">
+                                <a href="<?php echo get_category_link($category) ?>">
+                                    <?php echo $category->name; ?>
+                                </a>
+                            </li>
+                            <?php endforeach; ?>
+                        </ul>
+                        <?php endif;
 
                             ?>
-                </div>
-                <div class="card__post-data">
-                    <?php echo $author ?>
-                    &#183;
-                    <?php echo get_the_date("F d, Y") ?>
-                </div>
-                <div class="card__post-title">
-                    <a class="h4 new__title" href="<?php echo get_the_permalink() ?>"><?php echo get_the_title() ?></a>
-                </div>
+                    </div>
+                    <div class="card__post-data">
+                        <?php echo $author ?>
+                        &#183;
+                        <?php echo get_the_date("F d, Y") ?>
+                    </div>
+                    <div class="card__post-title">
+                        <a class="h4 new__title"
+                            href="<?php echo get_the_permalink() ?>"><?php echo get_the_title() ?></a>
+                    </div>
 
-                <div class="card__post-excerpt">
-                    <?php echo $excerpt; ?>
+                    <div class="card__post-excerpt">
+                        <?php echo $excerpt; ?>
+                    </div>
                 </div>
             </div>
+            <?php endwhile; ?>
         </div>
-        <?php endwhile; ?>
-    </div>
-    <?php $max_num_pages = $recent_posts->max_num_pages;
+        <?php $max_num_pages = $recent_posts->max_num_pages;
 
 
         if ($max_num_pages > 1) :
@@ -163,20 +172,20 @@ $placeholder = get_template_directory_uri() . '/images/rosenberg-logo.svg';
                 'type' => 'array'
             ));
         ?>
-    <div class="news_pagination">
-        <?php // news_pagination($max_num_pages, $recent_posts->found_posts, $paged == 0 ? 1 : $paged);; 
+        <div class="news_pagination">
+            <?php // news_pagination($max_num_pages, $recent_posts->found_posts, $paged == 0 ? 1 : $paged);; 
                 ?>
-    </div>
-    <div class="news__pagination">
+        </div>
+        <div class="news__pagination">
 
-        <?php user_pagination($max_num_pages, $recent_posts->found_posts, $paged == 0 ? 1 : $paged); ?>
-    </div>
+            <?php user_pagination($max_num_pages, $recent_posts->found_posts, $paged == 0 ? 1 : $paged); ?>
+        </div>
 
 
-    <?php endif; ?>
+        <?php endif; ?>
 
-    <?php wp_reset_postdata();  ?>
-    <?php endif; ?>
-    </div>
+        <?php wp_reset_postdata();  ?>
+        <?php endif; ?>
+        </div>
 </main>
 <?php get_footer(); ?>
