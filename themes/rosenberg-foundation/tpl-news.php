5ac42031a416b81a5377d114944b1a3d8db77d98
Nando Delgado <nando.emmanuel@gmail.com>
2022-04-09 20:38:05 -0300

    Fixed mereg conflict

--- themes/rosenberg-foundation/tpl-news.php
+++ themes/rosenberg-foundation/tpl-news.php
@@ -22,22 +22,79 @@ $args_recenpost = array(
 
 
 ?>
-<main class="page__news container">
+<main class="page__news">
 
-    <div class="page__news__header">
-        <h1><?php the_title(); ?></h1>
-        <?php get_search_form(array(
-            "echo" => true,
-            "aria_label" => 'Search news'
-        )); ?>
-    </div>
-    <?php
-    //if there isn't post show 
-    if (!$last_post) : ?>
-    <div class="news__nopost">
-        <h3>No results were found.</h3>
-    </div>
-    <?php else : ?>
+    <div class="container">
+        <div class="page__news__header">
+            <h1><?php the_title(); ?></h1>
+            <?php get_search_form(array(
+                "echo" => true,
+                "aria_label" => 'Search news'
+            )); ?>
+        </div>
+        <?php
+        //if there isn't post show 
+        if (!$recent_posts->have_posts()) : ?>
+        <div class="news__nopost">
+            <h3>No results were found.</h3>
+        </div>
+        <?php else : /* show last 12 post and last post like internal intro section */
+            $image = wp_get_attachment_image_srcset(get_post_thumbnail_id($last_post[0]["ID"]));
+        ?>
+        <div class="internal-intro-section news__last-post ">
+            <div class="internal-intro-section__img news__last-post__img">
+                <img srcset="<?php echo $image  ?>" alt="<?php echo esc_html($last_post[0]["post_title"]) ?>" />
+            </div>
+            <div class="internal-intro-section__content news__last-post__content">
+
+                <div class="news__last-post__container-text">
+                    <h3 class="h3"><?php echo esc_html($last_post[0]["post_title"]); ?></h3>
+                    <p>
+                        <a href="<?php echo get_permalink($last_post[0]['ID']); ?>">Read more <img
+                                src="<?php echo get_template_directory_uri() ?>/images/icons/icon-arrow-right-small-fill.svg"></a>
+                    </p>
+                </div>
+
+
+            </div>
+        </div>
+        <?php
+            if ($categories) : ?>
+        <div class="news__list-categories">
+            <ul class="news__list-categories__list">
+                <li class="news__list-categories__list__item news__list-categories__list__item--active">
+                    <a href="<?php get_the_permalink($post->ID); ?>">All</a>
+                </li>
+                <?php
+                        foreach ($categories as $category) :
+
+                        ?>
+                <li class="news__list-categories__list__item ">
+                    <a href="<?php echo get_category_link($category) ?>">
+                        <?php echo $category->name; ?>
+                    </a>
+                </li>
+                <?php endforeach ?>
+            </ul>
+        </div>
+        <?php
+                ?>
+        <?php endif
+            ?>
+
+        <div class="news__list">
+            <?php
+                while ($recent_posts->have_posts()) :
+                    $recent_posts->the_post();
+                    $image = get_the_post_thumbnail_url($recent_posts->post->ID, 'medium');
+
+                    $excerpt =  get_the_excerpt($recent_posts->post->ID) ? get_the_excerpt($recent_posts->post->ID) : substr(get_the_content(null, null, $recent_posts->post->ID), 0, 200);
+                    $excerpt  = wp_strip_all_tags($excerpt);
+                    $new_categories = wp_get_post_categories($recent_posts->post->ID);
+                    $author = get_the_author_meta("first_name") . " " .  get_the_author_meta("last_name");
+                    if ($author) {
+                        $author =  get_the_author_meta("display_name");
+                    }
 
     <?php
         /* choice last post as hero post */
@@ -52,6 +109,6 @@ $args_recenpost = array(
 
 
 
-    <?php endif; ?>
+            <?php endif; ?>
 </main>
 <?php get_footer(); ?>
