7320bdb3d9cc6d90d2043af69ee6d2968ceadfb0
el_gustavo <gustavowebs@gmail.com>
2022-04-09 02:02:26 -0300

    remove news prefix, is global now

--- themes/rosenberg-foundation/searchform.php
+++ themes/rosenberg-foundation/searchform.php
@@ -1,9 +1,9 @@
- <div class="search-container news__search__container" data-class="search-container">
-     <form role="search" method="get" id="searchform" class="news__search" action="<?php echo get_site_url() ?>">
+ <div class=" search__container" data-class="search-container">
+     <form role="search" method="get" id="searchform" class="search" action="<?php echo get_site_url() ?>">
          <label class="accessible-text" for="s">Search news</label>
-         <input class="news__search__input" data-class="search-input" name="s" id="s" type="search" placeholder="Search"
+         <input class="search__input" data-class="search-input" name="s" id="s" type="search" placeholder="Search"
              autocomplete="off" />
-         <button type="submit" class="news__search__button">
+         <button type="submit" class="search__button">
              <img src="<?php echo get_template_directory_uri() ?>/images/icon-search-fill.png">
          </button>
      </form>
