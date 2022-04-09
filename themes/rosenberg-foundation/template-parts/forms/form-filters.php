6815ff60a228610b43a33dd1fff5741a8edd2bc8
el_gustavo <gustavowebs@gmail.com>
2022-04-09 02:07:24 -0300

    add filter form with css and javascript

---
+++ themes/rosenberg-foundation/template-parts/forms/form-filters.php
@@ -0,0 +1,32 @@
+<div class="filters__container" data-class="search-container">
+    <div class="label__dropdown">
+        Filters
+    </div>
+    <div class="dropdown">
+        <input type="checkbox" class="dropdown__switch" id="filter-switch" hidden />
+        <label for="filter-switch" class="dropdown__options-filter">
+            <ul class="dropdown__filter" role="listbox" tabindex="-1">
+                <li class="dropdown__filter-selected" aria-selected="true">
+                    All
+                </li>
+                <li>
+                    <ul class="dropdown__select">
+                        <li class="dropdown__select-option" role="option">
+                            Leading Edge Fund
+                        </li>
+                        <li class="dropdown__select-option" role="option">
+                            Criminal and Youth Justice Reform
+                        </li>
+                        <li class="dropdown__select-option" role="option">
+                            Immigrant Rights and Inclusion
+                        </li>
+                        <li class="dropdown__select-option" role="option">
+                            Economic Justice
+                        </li>
+
+                    </ul>
+                </li>
+            </ul>
+        </label>
+    </div>
+</div>
