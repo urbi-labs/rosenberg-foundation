389eb600aea9656183280f5d9a4965ba29d7ddae
el_gustavo <gustavowebs@gmail.com>
2022-04-11 20:55:20 -0300

    use get template parts for modules

--- themes/rosenberg-foundation/tpl-news.php
+++ themes/rosenberg-foundation/tpl-news.php
@@ -6,87 +6,71 @@ Template Name: News
 get_header();
 
 $post_slug = $post->post_name;
-$args_last_post = array('numberposts' => '1');
-$last_post = wp_get_recent_posts($args_last_post, 1);
-$categories = get_categories();
+$args_hero_post = array('numberposts' => '1');
+$last_post = wp_get_recent_posts($args_hero_post, 1);
+
 
 $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
 $args_recenpost = array(
     'orderby' => 'date',
     'order' => 'desc',
-
     'paged' => $paged
 
 );
 
 
 ?>
-<main class="page__news">
+<main class="page__news container">
 
-    <div class="container">
-        <div class="page__news__header">
-            <h1><?php the_title(); ?></h1>
-            <?php get_search_form(array(
-                "echo" => true,
-                "aria_label" => 'Search news'
-            )); ?>
-        </div>
-        <?php
-        //if there isn't post show 
-        if (!$recent_posts->have_posts()) : ?>
-        <div class="news__nopost">
-            <h3>No results were found.</h3>
-        </div>
-        <?php else : /* show last 12 post and last post like internal intro section */
-            $image = wp_get_attachment_image_srcset(get_post_thumbnail_id($last_post[0]["ID"]));
-        ?>
-        <div class="internal-intro-section news__last-post ">
-            <div class="internal-intro-section__img news__last-post__img">
-                <img srcset="<?php echo $image  ?>" alt="<?php echo esc_html($last_post[0]["post_title"]) ?>" />
-            </div>
+    <div class="page__news__header">
+        <h1><?php the_title(); ?></h1>
+        <?php get_search_form(array(
+            "echo" => true,
+            "aria_label" => 'Search news'
+        )); ?>
+    </div>
+    <?php
+    //if there isn't post show 
+    if (!$last_post) : ?>
+    <div class="news__nopost">
+        <h3>No results were found.</h3>
+    </div>
+    <?php else : ?>
 
-            <div class="news__last-post__container-text">
-                <h3 class="h3"><?php echo esc_html($last_post[0]["post_title"]); ?></h3>
-                <p>
-                    <a href="<?php echo get_permalink($last_post[0]['ID']); ?>">Read more <img
-                            src="<?php echo get_template_directory_uri() ?>/images/icons/icon-arrow-right-small-fill.svg"></a>
-                </p>
-            </div>
+    <?php
 
-        </div>
-        <?php
-            if ($categories) : ?>
-        <div class="news__list-categories">
-            <ul class="news__list-categories__list">
-                <li class="news__list-categories__list__item news__list-categories__list__item--active">
-                    <a href="<?php get_the_permalink($post->ID); ?>">All</a>
-                </li>
-                <?php
-                        foreach ($categories as $category) :
+        if ($categories) : ?>
+    <div class="news__list-categories">
+        <ul class="news__list-categories__list">
+            <li class="news__list-categories__list__item news__list-categories__list__item--active">
+                <a href="<?php get_the_permalink($post->ID); ?>">All</a>
+            </li>
+            <?php
+                    foreach ($categories as $category) :
 
                         ?>
-                <li class="news__list-categories__list__item ">
-                    <a href="<?php echo get_category_link($category) ?>">
-                        <?php echo $category->name; ?>
-                    </a>
-                </li>
-                <?php endforeach ?>
-            </ul>
-        </div>
-        <?php
+            <li class="news__list-categories__list__item ">
+                <a href="<?php echo get_category_link($category) ?>">
+                    <?php echo $category->name; ?>
+                </a>
+            </li>
+            <?php endforeach ?>
+        </ul>
+    </div>
+    <?php
                 ?>
-        <?php endif
+    <?php endif
             ?>
 
-        <div class="news__list">
-            <?php
+    <div class="news__list">
+        <?php
             while ($recent_posts->have_posts()) :
                 $recent_posts->the_post();
                 $image =  wp_get_attachment_image_srcset(get_post_thumbnail_id($recent_posts->post->ID));
             ?>
-            <div class="news__list">
-                <?php
+        <div class="news__list">
+            <?php
                     while ($recent_posts->have_posts()) :
                         $recent_posts->the_post();
                         $image = get_the_post_thumbnail_url($recent_posts->post->ID, 'medium');
@@ -99,7 +83,6 @@ $args_recenpost = array(
                             $author =  get_the_author_meta("display_name");
                         }
 
-<<<<<<< HEAD
     <?php
         /* choice last post as hero post */
         echo get_template_part('template-parts/posts/post', "hero", array('hero_post' => $last_post[0]));
@@ -109,49 +92,44 @@ $args_recenpost = array(
 
         /** news post list with pagination */
         echo get_template_part('template-parts/posts/posts', "news-list", ['args' => $args_recenpost]);
-=======
-
 
 
                     ?>
-                <div class="card new__list__item">
-                    <div class="card__post-featured-image news__featured-image">
-                        <a href="<?php the_permalink(); ?>">
-                            <img <?php echo $image ? "srcset='" . $image . "'" : "src='" . $placeholder . "'"   ?>
-                                alt="<?php echo get_the_title($recent_posts->post->ID); ?>"
-                                class="<?php echo !$image ? "news__placeholder" : "" ?>">
-                        </a>
-                    </div>
-                    <div class="news__item__content">
-                        <div class="card__post-categories new__item__categories">
-                            <?php
+            <div class="card new__list__item">
+                <div class="card__post-featured-image news__featured-image">
+                    <a href="<?php the_permalink(); ?>">
+                        <img <?php echo $image ? "srcset='" . $image . "'" : "src='" . $placeholder . "'"   ?>
+                            alt="<?php echo get_the_title($recent_posts->post->ID); ?>"
+                            class="<?php echo !$image ? "news__placeholder" : "" ?>">
+                    </a>
+                </div>
+                <div class="news__item__content">
+                    <div class="card__post-categories new__item__categories">
+                        <?php
                                     if ($new_categories) : ?>
-                            <ul>
-                                <?php
-<<<<<<< HEAD
+                        <ul>
+                            <?ph
 
             <div class="card new__list__item">
                 <div class="card__post-featured-image news__featured-image">
                     <a href="<?php the_permalink(); ?>">
-                                <img <?php echo $image ? "srcset='" . $image . "'" : "src='" . $placeholder . "'"   ?>
-                                    alt="<?php echo get_the_title($recent_posts->post->ID); ?>"
-                                    class="<?php echo !$image ? "news__placeholder" : "" ?>">
-                                </a>
-                        </div>
-                        <div class="news__item__content">
-                            <div class="card__post-categories new__item__categories">
-                                <?php
+                            <img <?php echo $image ? "srcset='" . $image . "'" : "src='" . $placeholder . "'"   ?>
+                                alt="<?php echo get_the_title($recent_posts->post->ID); ?>"
+                                class="<?php echo !$image ? "news__placeholder" : "" ?>">
+                            </a>
+                    </div>
+                    <div class="news__item__content">
+                        <div class="card__post-categories new__item__categories">
+                            <?php
                             if ($new_categories) : ?>
-                                <ul>
-                                    <?php
+                            <ul>
+                                <?php
 
                                     foreach ($new_categories as $category_id) :
                                         $category = get_category($category_id);
                                     ?>
-                                    =======
-                                    foreach ($new_categories as $category_id) :
-                                    $category = get_category($category_id);
-                                    ?>
+                                <<<<<<< HEAD=======foreach ($new_categories as $category_id) :
+                                    $category=get_category($category_id); ?>
                                     >>>>>>> add template part hero, last post is an object.
 
                                     <li class=" ">
@@ -160,70 +138,70 @@ $args_recenpost = array(
                                         </a>
                                     </li>
                                     <?php endforeach; ?>
-                                </ul>
-                                <?php endif;
+                            </ul>
+                            <?php endif;
 
 <<<<<<< HEAD
                             ?>
-                            </div>
-                            <div class="card__post-title">
-                                <a class="h4 new__title"
-                                    href="<?php echo get_the_permalink() ?>"><?php echo get_the_title() ?></a>
-                            </div>
-                            <div class="card__post-data">
-                                <?php echo $author ?>
-                                &#183;
-                                <?php echo get_the_date("F d, Y") ?>
-                            </div>
-                            <div class="card__post-excerpt">
-                                <?php echo $excerpt; ?>
-                            </div>
-                            =======
-                            ?>
+                        </div>
+                        <div class="card__post-title">
+                            <a class="h4 new__title"
+                                href="<?php echo get_the_permalink() ?>"><?php echo get_the_title() ?></a>
                         </div>
                         <div class="card__post-data">
                             <?php echo $author ?>
                             &#183;
                             <?php echo get_the_date("F d, Y") ?>
                         </div>
-                        <div class="card__post-title">
-                            <a class="h4 new__title"
-                                href="<?php echo get_the_permalink() ?>"><?php echo get_the_title() ?></a>
-                        </div>
-
                         <div class="card__post-excerpt">
                             <?php echo $excerpt; ?>
-                            >>>>>>> add template part hero, last post is an object.
                         </div>
+                        =======
+                        ?>
+                    </div>
+                    <div class="card__post-data">
+                        <?php echo $author ?>
+                        &#183;
+                        <?php echo get_the_date("F d, Y") ?>
+                    </div>
+                    <div class="card__post-title">
+                        <a class="h4 new__title"
+                            href="<?php echo get_the_permalink() ?>"><?php echo get_the_title() ?></a>
                     </div>
 
-                    <li class=" ">
-                        <a href="<?php echo get_category_link($category) ?>">
-                            <?php echo $category->name; ?>
-                        </a>
-                    </li>
-                    <?php endforeach; ?>
-                    </ul>
-                    <?php endif;
+                    <div class="card__post-excerpt">
+                        <?php echo $excerpt; ?>
+                        >>>>>>> add template part hero, last post is an object.
+                    </div>
+                </div>
+
+                <li class=" ">
+                    <a href="<?php echo get_category_link($category) ?>">
+                        <?php echo $category->name; ?>
+                    </a>
+                </li>
+                <?php endforeach; ?>
+                </ul>
+                <?php endif;
 
                             ?>
-                </div>
-                <div class="card__post-data">
-                    <?php echo $author ?>
-                    &#183;
-                    <?php echo get_the_date("F d, Y") ?>
-                </div>
-                <div class="card__post-title">
-                    <a class="h4 new__title" href="<?php echo get_the_permalink() ?>"><?php echo get_the_title() ?></a>
-                </div>
-
-                <div class="card__post-excerpt">
-                    <?php echo $excerpt; ?>
-                </div>
             </div>
-            <?php endwhile; ?>
+            <div class="card__post-data">
+                <?php echo $author ?>
+                &#183;
+                <?php echo get_the_date("F d, Y") ?>
+            </div>
+            <div class="card__post-title">
+                <a class="h4 new__title" href="<?php echo get_the_permalink() ?>"><?php echo get_the_title() ?></a>
+            </div>
+
+            <div class="card__post-excerpt">
+                <?php echo $excerpt; ?>
+            </div>
         </div>
-        <?php $max_num_pages = $recent_posts->max_num_pages;
+        <?php endwhile; ?>
+    </div>
+    <?php $max_num_pages = $recent_posts->max_num_pages;
 
 
         if ($max_num_pages > 1) :
@@ -244,32 +222,82 @@ $args_recenpost = array(
 >>>>>>> add template part hero, last post is an object.
         ?>
 
-        <div class="news_pagination">
-            <?php // news_pagination($max_num_pages, $recent_posts->found_posts, $paged == 0 ? 1 : $paged);; 
+    <div class="news_pagination">
+        <?php // news_pagination($max_num_pages, $recent_posts->found_posts, $paged == 0 ? 1 : $paged);; 
                 ?>
-            <<<<<<< HEAD </div>
-                <div class="news__pagination">
-                    =======
-                </div>
-                <div class="news__pagination">
+        <<<<<<< HEAD </div>
+            <div class="news__pagination">
+                =======
+            </div>
+            <div class="news__pagination">
 
-                    <<<<<<< HEAD
-                        <?php user_pagination($max_num_pages, $recent_posts->found_posts, $paged == 0 ? 1 : $paged); ?>
-                        </div>
+                <<<<<<< HEAD
+                    <?php user_pagination($max_num_pages, $recent_posts->found_posts, $paged == 0 ? 1 : $paged); ?>
+                    </div>
 
 
 
-                        <?php endif; ?>
+                    <?php endif; ?>
 
-                        <?php wp_reset_postdata();  ?>
-                        <?php endif; ?>
+                    <?php wp_reset_postdata();  ?>
+                    <?php endif; ?>
+            </div>
+            >>>>>>> add template part hero, last post is an object.
+
+
+            <<<<<<< HEAD <?php endif; ?>==============<?php endif; ?> <?php wp_reset_postdata();  ?> <?php endif; ?>
                 </div>
                 >>>>>>> add template part hero, last post is an object.
+                >>>>>>> add template part hero, last post is an object.
+                =======
+
+                foreach ($new_categories as $category_id) :
+                $category = get_category($category_id);
+                ?>
 
 
-                <<<<<<< HEAD <?php endif; ?>==============<?php endif; ?> <?php wp_reset_postdata();  ?> <?php endif; ?>
-                    </div>
-                    >>>>>>> add template part hero, last post is an object.
-                    >>>>>>> add template part hero, last post is an object.
+                <li class=" ">
+                    <a href="<?php echo get_category_link($category) ?>">
+                        <?php echo $category->name; ?>
+                    </a>
+                </li>
+                <?php endforeach; ?>
+                </ul>
+                <?php endif;
+
+        /* choice last post as hero post */
+        echo get_template_part('template-parts/posts/post', "hero", array('hero_post' => $last_post[0]));
+
+        /* category list from posts*/
+        echo get_template_part('template-parts/posts/posts', 'categories');
+
+        /** news post list with pagination */
+        echo get_template_part('template-parts/posts/posts', "news-list", ['args' => $args_recenpost]);
+        ?>
+
+                <div class="news_pagination">
+                    <?php // news_pagination($max_num_pages, $recent_posts->found_posts, $paged == 0 ? 1 : $paged);; 
+                ?>
+                </div>
+                <div class="news__pagination">
+
+
+                    <?php user_pagination($max_num_pages, $recent_posts->found_posts, $paged == 0 ? 1 : $paged); ?>
+                </div>
+
+
+
+                <?php endif; ?>
+
+                <?php wp_reset_postdata();  ?>
+                <?php endif; ?>
+    </div>
+
+    <?php endif; ?>
+    </div>
+
+
+    <?php endif; ?>
+    >>>>>>> use get template parts for modules
 </main>
 <?php get_footer(); ?>
