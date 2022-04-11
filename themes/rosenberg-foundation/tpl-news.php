73c717f7dd6af71fa77c0163287b0a097de875e1
el_gustavo <gustavowebs@gmail.com>
2022-04-11 17:10:53 -0300

    post hero template and init post-news-list

--- themes/rosenberg-foundation/tpl-news.php
+++ themes/rosenberg-foundation/tpl-news.php
@@ -33,32 +33,15 @@ $placeholder = get_template_directory_uri() . '/images/rosenberg-logo.svg';
     </div>
     <?php
     //if there isn't post show 
-    if (!$recent_posts->have_posts()) : ?>
+    if (!$last_post) : ?>
     <div class="news__nopost">
         <h3>No results were found.</h3>
     </div>
     <?php else : /* show last 12 post and last post like internal intro section */
-        $image = wp_get_attachment_image_srcset(get_post_thumbnail_id($last_post[0]["ID"]));
+        echo get_template_part('template-parts/posts/post', "hero", array('hero_post' => $last_post[0]));
 
     ?>
-    <div class="internal-intro-section news__last-post ">
-        <div class="internal-intro-section__img news__last-post__img">
-            <img <?php echo $image ? "srcset='" . $image . "'" : "src='" . $placeholder . "'"   ?>
-                alt="<?php echo esc_html($last_post[0]["post_title"]) ?>" />
-        </div>
-        <div class="internal-intro-section__content news__last-post__content">
 
-            <div class="news__last-post__container-text">
-                <h3 class="h3"><?php echo esc_html($last_post[0]["post_title"]); ?></h3>
-                <p>
-                    <a href="<?php echo get_permalink($last_post[0]['ID']); ?>">Read more <img
-                            src="<?php echo get_template_directory_uri() ?>/images/icon-arrow-right-small-fill.png"></a>
-                </p>
-            </div>
-
-
-        </div>
-    </div>
     <?php
         if ($categories) : ?>
     <div class="news__list-categories">
