a226fa7b8d1223bbd742402bf565c46a68ebee12
el_gustavo <gustavowebs@gmail.com>
2022-04-12 21:37:04 -0300

    Replace content by template parts

--- themes/rosenberg-foundation/tpl-our-grantees.php
+++ themes/rosenberg-foundation/tpl-our-grantees.php
@@ -10,15 +10,25 @@ Template Name: Our grantees
  * get from news for test
  */
 
+
+$post_slug = $post->post_name;
+$args_hero_post = array('numberposts' => '1', 'post_type' => 'grantee');
+$last_post = wp_get_recent_posts($args_hero_post, 1);
+
+
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
 $args_recenpost = array(
     'orderby' => 'date',
     'order' => 'desc',
-    'posts_per_page' => 6,
+    'post_type' => 'grantee',
     'paged' => $paged
 
 );
-$recent_posts = new WP_Query($args_recenpost);
-$placeholder = get_template_directory_uri() . '/images/rosenberg-logo.svg';
+
+$categories = get_categories(array('taxonomy' => 'grantee-category', 'hide_empty' => false));
+
+
 ?>
 <main class="our-grantees">
     <h1 class="our-grantees__title"><?php the_title(); ?></h1>
@@ -42,66 +52,10 @@ $placeholder = get_template_directory_uri() . '/images/rosenberg-logo.svg';
                 "aria_label" => 'Search news',
                 "post_type" => 'grantee'
             )); ?>
-            <?php echo get_template_part("template-parts/forms/form", "filters", array('post_type' => 'grantee')) ?>
+            <?php echo get_template_part("template-parts/forms/form", "filters", array('categories' => $categories)) ?>
         </div>
-        <div class="news__list">
-            <?php
-            while ($recent_posts->have_posts()) :
-                $recent_posts->the_post();
-                $image =  wp_get_attachment_image_srcset(get_post_thumbnail_id($recent_posts->post->ID));
+        <?php echo get_template_part('template-parts/posts/posts', "grantees-list", ['args' => $args_recenpost]); ?>
 
-                $excerpt =  get_the_excerpt($recent_posts->post->ID) ? get_the_excerpt($recent_posts->post->ID) : substr(get_the_content(null, null, $recent_posts->post->ID), 0, 200);
-                $excerpt  = wp_strip_all_tags($excerpt);
-                $new_categories = wp_get_post_categories($recent_posts->post->ID);
-                $author = get_the_author_meta("first_name") . " " .  get_the_author_meta("last_name");
-                if ($author) {
-                    $author =  get_the_author_meta("display_name");
-                }
-
-
-            ?>
-            <div class="card new__list__item">
-                <div class="card__post-featured-image news__featured-image">
-                    <a href="<?php the_permalink(); ?>">
-                        <img <?php echo $image ? "srcset='" . $image . "'" : "src='" . $placeholder . "'"   ?>
-                            alt="<?php echo get_the_title($recent_posts->post->ID); ?>"
-                            class="<?php echo !$image ? "news__placeholder" : "" ?>">
-                    </a>
-                </div>
-                <div class="news__item__content">
-                    <div class="card__post-categories new__item__categories">
-                        <?php
-                            if ($new_categories) : ?>
-                        <ul>
-                            <?php
-                                    foreach ($new_categories as $category_id) :
-                                        $category = get_category($category_id);
-                                    ?>
-
-                            <li class=" ">
-                                <a href="<?php echo get_category_link($category) ?>">
-                                    <?php echo $category->name; ?>
-                                </a>
-                            </li>
-                            <?php endforeach; ?>
-                        </ul>
-                        <?php endif;
-
-                            ?>
-                    </div>
-
-                    <div class="card__post-title">
-                        <a class="h5 new__title"
-                            href="<?php echo get_the_permalink() ?>"><?php echo get_the_title() ?></a>
-                    </div>
-
-                    <div class="card__post-excerpt">
-                        <?php echo $excerpt; ?>
-                    </div>
-                </div>
-            </div>
-            <?php endwhile; ?>
-        </div>
     </div>
 </main>
 
