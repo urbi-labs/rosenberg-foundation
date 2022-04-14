2d0064543d60e04f7060c681a929c5521929f8df
el_gustavo <gustavowebs@gmail.com>
2022-04-14 16:21:43 -0300

    move post-people to post-single-person template part

--- themes/rosenberg-foundation/template-parts/posts/post-people.php
+++
@@ -1,2 +0,0 @@
-<?php 
-    $image = get_the_post_thumbnail_url($posts->post->ID, 'medium');
---
+++ themes/rosenberg-foundation/template-parts/posts/post-single-person.php
@@ -0,0 +1,34 @@
+<?php
+$image = wp_get_attachment_image_srcset(get_post_thumbnail_id(get_the_ID()));
+$placeholder = get_template_directory_uri() . '/images/placeholder-80.jpg';
+$role = get_field('role', get_the_ID());
+
+?>
+<div class="person-profile">
+    <div class="person-profile__featured-image">
+        <img <?php echo $image ? "srcset='" . $image . "'" : "src='" . $placeholder . "'"   ?>
+            alt="<?php echo esc_html(get_the_title()); ?>" class="">
+    </div>
+    <div class="person-profile__content">
+        <div class="person-profile__title">
+            <div class="title__container">
+
+                <span class="h2 title__blue-bg"><?php the_title(); ?>
+
+                </span><br />
+                <?php
+                if ($role) : ?>
+                <span class="h4 title__blue-bg"><?php echo $role; ?> </span>
+                <?php endif;
+                ?>
+            </div>
+
+            <div class="person-profile__text">
+                <div class="text__gray-bg p-mid">
+                    <?php echo the_content(); ?>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
