2fd2700661490001548faa951427b0616f48ff6d
Nando Delgado <nando.emmanuel@gmail.com>
2022-04-15 17:20:16 -0300

    Removed webmanifest file

--- themes/rosenberg-foundation/header.php
+++ themes/rosenberg-foundation/header.php
@@ -15,7 +15,6 @@
     <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory'); ?>/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_directory'); ?>/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_directory'); ?>/favicon/favicon-16x16.png">
-    <link rel="manifest" href="<?php bloginfo('template_directory'); ?>/favicon/site.webmanifest">
     <?php wp_head(); ?>
 </head>
 
