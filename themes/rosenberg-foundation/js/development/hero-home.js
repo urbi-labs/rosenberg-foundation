484eacb1b3784e0f2672e4157b10e8d529343cbd
Nando Delgado <nando.emmanuel@gmail.com>
2022-04-18 20:51:05 -0300

    Corrected header button and slider speed

--- themes/rosenberg-foundation/js/development/hero-home.js
+++ themes/rosenberg-foundation/js/development/hero-home.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        speed: 500,
+        speed: 2000,
         autoplaySpeed: 4000,
         prevArrow: $('.hero-home__prev-arrow.slick-prev'),
         nextArrow: $('.hero-home__next-arrow.slick-next')
