a57446a227199db3e4c8f342d2f4442c34731e03
el_gustavo <gustavowebs@gmail.com>
2022-04-19 17:43:38 -0300

    add taxonomy args for news post block

--- themes/rosenberg-foundation/tpl-news.php
+++ themes/rosenberg-foundation/tpl-news.php
@@ -8,16 +8,27 @@ get_header();
 $post_slug = $post->post_name;
 $args_hero_post = array('numberposts' => '1');
 $last_post = wp_get_recent_posts($args_hero_post, 1);
+$slug = isset($_GET['category']) ? $_GET['category'] : "";
 
 
 $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 $args_recenpost = array(
     'orderby' => 'date',
     'order' => 'desc',
-    'paged' => $paged
+    'paged' => $paged,
+
 
 );
 
+if ($slug != "") {
+    $args_recenpost['tax_query'] = array(
+        array(
+            'taxonomy' => 'category',
+            'field'    => 'slug',
+            'terms'    => $slug,
+        ),
+    );
+}
 
 ?>
 <main class="page__news container">
@@ -42,7 +53,7 @@ $args_recenpost = array(
         echo get_template_part('template-parts/posts/post', "hero", array('hero_post' => $last_post[0]));
 
         /* category list from posts*/
-        echo get_template_part('template-parts/posts/posts', 'categories');
+        echo get_template_part('template-parts/posts/posts', 'categories', array('all' => true, 'current_slug' => $slug));
 
         /** news post list with pagination */
         echo get_template_part('template-parts/posts/posts', "news-list", ['args' => $args_recenpost]);
