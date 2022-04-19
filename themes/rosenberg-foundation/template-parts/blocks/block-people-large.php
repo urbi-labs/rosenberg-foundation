ea75bf73d102d56f4a7efdcf411a1546734a06fd
el_gustavo <gustavowebs@gmail.com>
2022-04-19 16:40:57 -0300

    filter person only staff

--- themes/rosenberg-foundation/template-parts/blocks/block-people-large.php
+++ themes/rosenberg-foundation/template-parts/blocks/block-people-large.php
@@ -2,31 +2,40 @@
 
 /** Get person custom post type */
 $args = array(
-    'post_type' => 'person'
+    'post_type' => 'person',
+    'tax_query' => array(
+        array(
+            'taxonomy' => 'person-category',
+            'field'    => 'slug',
+            'terms'    => 'staff',
+        ),
+    ),
 );
 
 $people = new WP_Query($args);
 $index = 0;
 ?>
 
-<?php // echo get_template_part('template-parts/posts/posts', 'categories'); ?>
+<?php // echo get_template_part('template-parts/posts/posts', 'categories'); 
+?>
 
 <?php
 
-if($people->have_posts()) : while($people->have_posts()) : $people->the_post();
-$image_right = $index % 2 === 0;
+if ($people->have_posts()) : while ($people->have_posts()) : $people->the_post();
+        $image_right = $index % 2 === 0;
 
 ?>
 
 <div class="overlapping-cards__container card-link <?php echo $image_right ? 'image-right' : '' ?> block-people-large">
 
     <div class="overlapping-cards__image card-link-image">
-        <img srcset="<?php echo wp_get_attachment_image_srcset(get_post_thumbnail_id(get_the_ID())) ?>" class="overlapping-cards__image__image">
+        <img srcset="<?php echo wp_get_attachment_image_srcset(get_post_thumbnail_id(get_the_ID())) ?>"
+            class="overlapping-cards__image__image">
     </div>
     <div class="overlapping-cards__text <?php echo $image_right ? 'image-right' : '' ?>">
         <h3 class="overlapping-cards__text-heading"><?php echo get_the_title() ?></h3>
         <p class="block-people-large__position"><?php echo get_field('role', get_the_ID()) ?></p>
-        <?php if(!empty(get_the_excerpt())) : ?>
+        <?php if (!empty(get_the_excerpt())) : ?>
         <div class="block-people-large__excerpt">
             <?php echo get_the_excerpt() ?>
         </div>
@@ -40,6 +49,7 @@ $image_right = $index % 2 === 0;
 </div>
 
 <?php
-$index ++;
-endwhile; endif; 
+        $index++;
+    endwhile;
+endif;
 ?>
