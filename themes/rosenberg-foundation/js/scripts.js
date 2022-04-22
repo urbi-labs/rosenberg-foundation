ccf06eb68ab01a68c647b9b98629df92cb48286b
el_gustavo <gustavowebs@gmail.com>
2022-04-21 22:09:26 -0300

    Add leadingfund fields

--- themes/rosenberg-foundation/js/scripts.js
+++ themes/rosenberg-foundation/js/scripts.js
@@ -135,6 +135,31 @@ $(document).ready(function(){
     });
 });
 
+$(document).ready(function () {
+    var open_ledge = $('.menu__leadingedgefund>a');
+    var block_ledge = $('#block-leadingedgefund');
+
+    var overlay = $('.mobile-menu--overlay');
+    var mobile_menu = $('#mobile__menu');
+
+
+
+    open_ledge.each(function (index) {
+        $(this).on('click', function (e) {
+
+            e.preventDefault();
+            block_ledge.addClass('block-leadingedgefund--show');
+            overlay.removeClass('mobile-menu--overlay--show');
+            mobile_menu.removeClass('mobile__menu--show');
+        });
+    });
+    $(".close_leadingedgefund").on('click', function (e) {
+        e.preventDefault();
+
+        block_ledge.removeClass('block-leadingedgefund--show')
+    })
+});
+
 $(function () {
 
     var open_button = $('.mobile__burger-button');
